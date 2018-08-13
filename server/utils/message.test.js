0c4acb25c092545d66078d1bfabc5b09b8090d34
neal.bharucha@msichicago.org <nealbharucha@yahoo.com>
2018-08-12 20:44:06 -0500

    Add geolocation support via geolocation api w/ test

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-const { generateMessage } = require('./message.js');
+const { generateMessage, generateLocationMessage } = require('./message.js');
 
 describe('generateMessage', () => {
     it('should generate the correct message object', () => {
@@ -10,5 +10,16 @@ describe('generateMessage', () => {
             text: 'Test Message'
         });
         expect(message.createdAt).toBeA('number');
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        const message = generateLocationMessage(`Neal`, 1, 2);
+        expect(message).toInclude({
+            from: `Neal`,
+            url: `https://www.google.com/maps?q=1,2`
+        });
+        expect(message.createdAt).toBeA(`number`);
     });
 });
