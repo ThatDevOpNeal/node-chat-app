784e014161317235133f6e525c35edd1054d4435
neal.bharucha@msichicago.org <nealbharucha@yahoo.com>
2018-08-14 01:10:11 -0500

    Style the program, partially

--- public/js/index.js
+++ public/js/index.js
@@ -30,11 +30,13 @@ socket.on('newLocationMessage', function (message) {
 jQuery(`#message-form`).on(`submit`, function (e) {
     e.preventDefault();
 
+    const messageTextBox = jQuery(`[name=message]`);
+
     socket.emit(`createMessage`, {
         from: `User`,
-        text: jQuery(`[name=message]`).val()
+        text: messageTextBox.val()
     }, function () {
-        
+        messageTextBox.val(``);
     });
 });
 
@@ -44,13 +46,17 @@ locationButton.on(`click`, function () {
         return alert(`Geolocation not supported by this browser.`);
     }
 
+    locationButton.attr(`disabled`, `disabled`).text(`Sending Location...`);
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr(`disabled`).text(`Send Location`);
         socket.emit(`createLocationMessage`, {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
         
     }, function (err) {
+        locationButton.removeAttr(`disabled`).text(`Send Location`);
         alert(`Unable to fetch location`);
     });
 });
